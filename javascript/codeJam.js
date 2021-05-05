fbb7da817cb1b6431476a993687b7403750298fe
ShaishavMaisuria <smaisuri@uncc.edu>
2021-05-05 13:52:24 -0400

    aster

--- javascript/codeJam.js
+++ javascript/codeJam.js
@@ -1,3 +1,5 @@
+// this is request to json data in order to display on the ui 
+// mainly used for AJAX requirement -> 1
 $(document).ready(function() {
 	$("#tabs").accordion({collapsible:true,
 		heightStyle:"content"});
@@ -30,33 +32,34 @@ $(document).ready(function() {
 		}
 	});
 
-
-	$.ajax({
-		type: "get",
-		url: "codeJamGuidelines.json",
-		beforeSend: function() {
-			$("#guidelines").html("Loading... codeJam Guidelines");
-		},
-		timeout: 10000,
-		error: function(json, status, error) {
-			alert("Error: " + json.status + " - " + error);
-		},
-		dataType: "json",
-		success: function(data) {
-			$("#guidelines").html("");
-			$.each(data,function(){
-				
-				
-				$.each(this,function(key,value){
-					var x="";
-					for(i=0;i< value.content.length;i++){
-						x+="<li>"+value.content[i]	+"</li>";
-					}
-					$("#guidelines").append("<h3> "+value.Header+"</h3>" + 
-						"<ol>"+x+"</ol>"
-						+"<hr>");
-				});
+// this is request to json data in order to display on the ui 
+// mainly used for AJAX requirement -> 1
+$.ajax({
+	type: "get",
+	url: "codeJamGuidelines.json",
+	beforeSend: function() {
+		$("#guidelines").html("Loading... codeJam Guidelines");
+	},
+	timeout: 10000,
+	error: function(json, status, error) {
+		alert("Error: " + json.status + " - " + error);
+	},
+	dataType: "json",
+	success: function(data) {
+		$("#guidelines").html("");
+		$.each(data,function(){
+			
+			
+			$.each(this,function(key,value){
+				var x="";
+				for(i=0;i< value.content.length;i++){
+					x+="<li>"+value.content[i]	+"</li>";
+				}
+				$("#guidelines").append("<h3> "+value.Header+"</h3>" + 
+					"<ol>"+x+"</ol>"
+					+"<hr>");
 			});
-		}
-	});
+		});
+	}
+});
 });
--- javascript/contact.js
+++ javascript/contact.js
@@ -1,5 +1,5 @@
-
-
+// using this to create a toggling effect to contact button when clicked like a small animation
+// also a Jquery plugin requirment js
 $(document).ready(function(){
 	$(".contact-btn").click(function(){
 		
@@ -7,8 +7,8 @@ $(document).ready(function(){
 	});
 });
 
-
-
+// it is used to make provide specific functionality to the date picker
+// also a Jquery plugin requirment js
 $(document).ready(function(){
 	$("#datepicker").datepicker({
 		changeMonth:true,
